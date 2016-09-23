4b74b5d0fc087c08e6705d6a74b156e7ae0ffc73
yelper <sarikaya@cs.wisc.edu>
2016-09-23 12:34:05 -0500

    Removing extraneous files from gh-pages branch.

--- index.js
+++
@@ -1,6 +0,0 @@
-export {default as scatterplot} from "./src/scatterplot";
-export {default as scatterplot_webgl} from "./src/scatterplot_webgl"; 
-export {default as objectlist} from "./src/objectlist";
-export {default as dropdown} from "./src/dropdown";
-export {default as legend} from "./src/legend";
-export {default as twoDimFactory} from "./src/twoDimFactory";
